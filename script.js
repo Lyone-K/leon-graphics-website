c7914886d649147b2ea4295e4348deda3e2d161e
Lyone-K <105782235+Lyone-K@users.noreply.github.com>
2025-10-23 22:49:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ fadeElements.forEach(el => {
   appearOnScroll.observe(el);
 });
 
-<script type="module">
+<script type="module" src="./script.js"></script>
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-analytics.js";
@@ -89,3 +89,4 @@ document.addEventListener("DOMContentLoaded", () => {
   updateAverage();
 });
 </script>
+
